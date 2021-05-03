0d75b30abb8647d637f519e968cb5ee5501323ee
Julian Zimmermann <54834203+julianzimmermann@users.noreply.github.com>
2021-05-03 07:26:45 +0200

    Bugfix (#3)
    - Throw exception if response code is not 200, 201 or 202

--- src/FondOfSpryker/Zed/JellyfishSalesOrder/Business/Api/Adapter/SalesOrderAdapter.php
+++ src/FondOfSpryker/Zed/JellyfishSalesOrder/Business/Api/Adapter/SalesOrderAdapter.php
@@ -2,13 +2,21 @@
 
 namespace FondOfSpryker\Zed\JellyfishSalesOrder\Business\Api\Adapter;
 
+use Exception;
 use FondOfSpryker\Zed\Jellyfish\Business\Api\Adapter\AbstractAdapter as FondOfSprykerJellyfishAbstractAdapter;
 use Psr\Http\Message\ResponseInterface;
 use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
+use Symfony\Component\HttpFoundation\Response;
 
 class SalesOrderAdapter extends FondOfSprykerJellyfishAbstractAdapter implements SalesOrderAdapterInterface
 {
     protected const ORDERS_URI = 'standard/orders';
+
+    protected const VALID_CODES = [
+        Response::HTTP_OK,
+        Response::HTTP_CREATED,
+        Response::HTTP_ACCEPTED
+    ];
 
     /**
      * @return string
@@ -19,12 +27,16 @@ class SalesOrderAdapter extends FondOfSprykerJellyfishAbstractAdapter implements
     }
 
     /**
-     * @param \Psr\Http\Message\ResponseInterface $response
-     * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer $transfer
+     * @param  \Psr\Http\Message\ResponseInterface  $response
+     * @param  \Spryker\Shared\Kernel\Transfer\AbstractTransfer|\Generated\Shared\Transfer\JellyfishOrderTransfer  $transfer
      *
      * @return void
      */
     protected function handleResponse(ResponseInterface $response, AbstractTransfer $transfer): void
     {
+        if (in_array($response->getStatusCode(), self::VALID_CODES, true) === false) {
+            throw new Exception(sprintf('Order export in store %s for order with id %s and reference %s failed!',
+                $transfer->getStore(), $transfer->getId(), $transfer->getReference()));
+        }
     }
 }
